5f9636f70e6bc6f9eee2bbd2ff0c472c37925340
Stark561 <123174593+Stark561@users.noreply.github.com>
2023-05-11 00:01:37 +0300

    fwff

--- src/index.js
+++ src/index.js
@@ -2,3 +2,15 @@ import axios from "axios";
 import simpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import Notiflix from "notiflix";
+
+const form = document.querySelector('.search-form');
+const input = form.elements.searchQuery;
+const btn = form.elements.submitBtn;
+const gallery = document.querySelector('.gallery');
+const loader = document.querySelector('#loader');
+const scrollUp = document.querySelector('.scroll-up');
+form.addEventListener('submit', onBtnSubmit);
+let page = 1;
+let totalImages = '';
+let observer;
+let isFormSubmitted = false;
