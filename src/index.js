0ea477b5137f2f0c7320033fa84f77579f8c020c
Stark561 <123174593+Stark561@users.noreply.github.com>
2023-05-11 13:53:14 +0300

    info

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import simpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import Notiflix from "notiflix";
 import smoothScroll from './js/smoothScroll.js';
+import renderMarkup from './js/renderMarkup.js';
 
 const form = document.querySelector('.header');
 const loader = document.querySelector('#loader');
@@ -21,43 +22,43 @@ const options = {
 
   let lightbox = new simpleLightbox('.gallery a');
 
-  async function loadContent() {
-  const inputValue = input.value;
-  const API_KEY = '36186802-862f6fad69a85448277218aac';
-  const BASE_URL = `https://pixabay.com/api`;
-  const END_POINT = `/?key=${API_KEY}&q=${inputValue}=&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
-  const url = BASE_URL + END_POINT;
-  try {
-  const response = await axios.get(url);
-  totalImages = response.data.totalHits;
-  if (totalImages === 0) {
-    return Notiflix.Notify.failure('К сожалению, нет изображений, соответствующих вашему поисковому запросу. Пожалуйста, попробуйте еще раз.');
-  } 
-  if (!isFormSubmitted) {
-    Notiflix.Notify.success(`Ура! Мы нашли ${totalImages} изображений.`);
-    isFormSubmitted = true;
-  }
-
-  const images = response.data.hits.map(image => ({
-  webformatURL: image.webformatURL,
-  largeImageURL: image.largeImageURL,
-  tags: image.tags,
-  likes: image.likes,
-  views: image.views,
-  comments: image.comments,
-  downloads: image.downloads
-  }))
-
-  gallery.insertAdjacentHTML('beforeend', renderMarkup(images));
-  lightbox.refresh();
-  const firstChild = gallery.firstElementChild;
-    if (firstChild) { 
-      setTimeout(smoothScroll, 400);
+    async function loadContent() {
+    const inputValue = input.value;
+    const API_KEY = '36186802-862f6fad69a85448277218aac';
+    const BASE_URL = `https://pixabay.com/api`;
+    const END_POINT = `/?key=${API_KEY}&q=${inputValue}=&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
+    const url = BASE_URL + END_POINT;
+    try {
+    const response = await axios.get(url);
+    totalImages = response.data.totalHits;
+    if (totalImages === 0) {
+      return Notiflix.Notify.failure('К сожалению, нет изображений, соответствующих вашему поисковому запросу. Пожалуйста, попробуйте еще раз.');
+    } 
+    if (!isFormSubmitted) {
+      Notiflix.Notify.success(`Ура! Мы нашли ${totalImages} изображений.`);
+      isFormSubmitted = true;
     }
-  } catch (err) {
-    Notiflix.Notify.failure("Сожалеем, но вы достигли конца результатов поиска.");
+  
+    const images = response.data.hits.map(image => ({
+    webformatURL: image.webformatURL,
+    largeImageURL: image.largeImageURL,
+    tags: image.tags,
+    likes: image.likes,
+    views: image.views,
+    comments: image.comments,
+    downloads: image.downloads
+    }))
+  
+    gallery.insertAdjacentHTML('beforeend', renderMarkup(images));
+    lightbox.refresh();
+    const firstChild = gallery.firstElementChild;
+      if (firstChild) { 
+        setTimeout(smoothScroll, 400);
+      }
+    } catch (err) {
+      Notiflix.Notify.failure("Сожалеем, но вы достигли конца результатов поиска.");
+    }
   }
-}
 
 function onBtnSubmit(e) {
   e.preventDefault();
@@ -75,30 +76,4 @@ function onBtnSubmit(e) {
     }
   }, options);
   observer.observe(loader);
- }
-
- function renderMarkup(data) {
- return data.map(({webformatURL,largeImageURL,tags, likes, views, comments, downloads}) => 
- `<a href="${largeImageURL}"><div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b>
-      ${likes}
-    </p>
-    <p class="info-item">
-      <b>Views</b>
-      ${views}
-    </p>
-    <p class="info-item">
-      <b>Comments</b>
-      ${comments}
-    </p>
-    <p class="info-item">
-      <b>Downloads</b>
-      ${downloads}
-    </p>
-  </div>
-</div></a>`
- ).join('')
-};
+ };
